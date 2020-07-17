4e8645cd1fffc2a019b649db3eb3ecc2bb9e4d71
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-07-17 17:53:23 +0200

    Update cameras.js

--- api/cameras.js
+++ api/cameras.js
@@ -1,11 +1,28 @@
-let cameras = new XMLHttpRequest()
+let camerasUrl = 'http://localhost:3000/api/cameras'
+if (window.fetch) {
+    //fetch mode
+    fetch(camerasUrl)
+        .then((response) => {
+            response.json().then((data) => {
 
-cameras.onreadystatechange = function(){
-    if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
-        localStorage.setItem('cameras',this.responseText)
-        return
+                console.log(data)
+                let test = JSON.stringify(data)
+                localStorage.setItem('cameras', test)
+            })
+        }).catch((err) =>
+            console.log(err))
+} else {
+    //XMLHttpRequest mode si fetch n'est pas supporté
+    let cameras = new XMLHttpRequest()
+
+    cameras.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            localStorage.setItem("cameras", this.responseText)
+            return
+        }
+
     }
 
+    cameras.open('GET', camerasUrl)
+    cameras.send()
 }
-cameras.open('GET','http://localhost:3000/api/cameras')
-cameras.send()
