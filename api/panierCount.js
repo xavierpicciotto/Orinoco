973fff74bf9d8608d937cb6e6b30af9710588fbf
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-07-24 17:03:40 +0200

    Create panierCount.js

---
+++ api/panierCount.js
@@ -0,0 +1,15 @@
+//affichage du nombre de produit(s) dans le panier
+//recuperation du panier
+if (localStorage.getItem('saveOrderList') != null) {
+    orderList = JSON.parse(localStorage.getItem('saveOrderList'))
+} else {
+    orderList = []
+}
+//calcule de la quantité de produit(s) dans le panier
+let cartCount = 0
+for (let i = 0; i != orderList.length; i++) {
+    let productCount = Number(orderList[i].amount)
+    cartCount += productCount
+}
+//mise en page de la quantité de produit(s) du panier
+document.getElementById('nav-cart-count').textContent = cartCount
